58b981570f847993b7d4ad2b8500c5a42bba155e
hoangcnttbkdn <hoangcnttbkdn@gmail.com>
2022-11-25 03:12:26 +0700

    ok ok

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 3000
 
 app.get('/', (req, res) => {
   var a = 1
-  res.send('okkkkkkkkkkkkkkk')
+  res.send('To additionally remove any stopped containers and all unused images (not just dangling images), add the -a flag to the command')
 })
 
 app.listen(port, () => {
