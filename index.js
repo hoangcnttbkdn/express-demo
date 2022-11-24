eb74fcf1b5f04f4246d0b445beac3b8440848211
hoangcnttbkdn <hoangcnttbkdn@gmail.com>
2022-11-25 02:42:52 +0700

    feature7 ne

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 3000
 
 app.get('/', (req, res) => {
   var a = 1
-  res.send('test oke lun ne')
+  res.send('7777777777777777777777 Ronaldo cr77777777777777777777')
 })
 
 app.listen(port, () => {
